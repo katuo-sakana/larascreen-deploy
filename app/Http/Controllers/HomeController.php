310258d7734e899de34fd34b4910042494e47349
katuo-sakana <katuo.webkaihatsu@gmail.com>
2021-01-03 03:40:46 +0900

    Unexpected identifierエラー

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,10 +29,10 @@ class HomeController extends Controller
         Storage::makeDirectory($random_directory);
         $directory_path = storage_path("app/public/{$random_directory}");
 
-        $puppeteer = new Puppeteer;
-        // $puppeteer = new Puppeteer([
-        //     'executable_path' => '/path/to/node',
-        // ]);
+        // $puppeteer = new Puppeteer;
+        $puppeteer = new Puppeteer([
+            'executable_path' => '/home/ec2-user/.nvm/versions/node/v12.18.2/bin/node',
+        ]);
         // $browser = $puppeteer->launch();
         $browser = $puppeteer->launch([
             'executablePath' => env('CHROMIUM_PATH'),
